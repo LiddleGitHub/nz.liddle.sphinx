31936547f3e335d7daff0b268c609571b1a46e3e
m on arch1 <marcus.liddle+git@gmail.com>
2025-10-27 23:55:41 +1300

    0.0.2 setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-######!/bin/bash
-#git remote remove origin
-#git remote add origin github.com:LiddleGitHub/nz.liddle.sphinx.git
-#pip install Sphinx
-#pip install sphinx sphinx_rtd_theme
-#touch Docs
-#cd Docs
-#sphinx-quickstart
+#!/bin/bash
+git remote remove origin
+git remote add origin git@github.com:LiddleGitHub/nz.liddle.sphinx.git
+pip install Sphinx
+pip install sphinx sphinx_rtd_theme
+touch Docs
+cd Docs
+sphinx-quickstart
